85049d71428c6118a6daf5ac6cb0a9cf5f66b794
krutikasawarkar <krutikasawarkar21@gmail.com>
2023-01-22 16:34:14 +0530

    Added migration.

---
+++ Mango.Services.Identity/Migrations/20230122110210_Seed_Identity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.Identity.Migrations
+{
+    public partial class Seed_Identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
